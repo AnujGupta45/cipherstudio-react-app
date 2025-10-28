792f4b2247e0d3f3a4ac763b6b5ff9642fe0235b
Amlor Anuj Kumar Gupta <119693219+AnujGupta45@users.noreply.github.com>
2025-10-28 20:30:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CipherSchools!</h1>
+      <h2>Namasthe!
+      Jai Shree Ram</h2>
+    </div>
+  )
+}
